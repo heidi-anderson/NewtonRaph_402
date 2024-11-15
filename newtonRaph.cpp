e9a124d6bbfe38ba1c6dfcc1738f0cb9b170fb97
101061875 <heidi.anderson@mines.sdsmt.edu>
2024-11-15 11:46:50 -0700

    theres an issue where it's going out of bounds or something idk. i tried to resize everything but it's still not working.

--- newtonRaph.cpp
+++ newtonRaph.cpp
@@ -2,15 +2,17 @@
 #include <vector>
 #include <cmath>
 #include <Eigen/Dense>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 using namespace Eigen;
 
-MatrixXd theGradient(MatrixXd X, MatrixXd Y, VectorXd B);
-MatrixXd theHessian(MatrixXd X, MatrixXd Y, VectorXd B);
-VectorXd newtonRaphson(VectorXd B, MatrixXd X, MatrixXd Y, double tol);
+MatrixXd theGradient(MatrixXd X, VectorXd Y, VectorXd B);
+MatrixXd theHessian(MatrixXd X, VectorXd Y, VectorXd B);
+VectorXd newtonRaphson(VectorXd B, MatrixXd X, VectorXd Y, double tol);
 
-VectorXd newtonRaphson(VectorXd B, MatrixXd X, MatrixXd Y, double tol)
+VectorXd newtonRaphson(VectorXd B, MatrixXd X, VectorXd Y, double tol)
 {
     MatrixXd gradient;
     MatrixXd hessian;
@@ -36,7 +38,7 @@ VectorXd newtonRaphson(VectorXd B, MatrixXd X, MatrixXd Y, double tol)
 
 }
 
-MatrixXd theGradient(MatrixXd X, MatrixXd Y, VectorXd B)
+MatrixXd theGradient(MatrixXd X, VectorXd Y, VectorXd B)
 {
     VectorXd regressionLine = X.transpose() * B;
     ArrayXd probs = 1 / (1 + (-regressionLine).array().exp()); 
@@ -45,7 +47,7 @@ MatrixXd theGradient(MatrixXd X, MatrixXd Y, VectorXd B)
     return result;
 }
 
-MatrixXd theHessian(MatrixXd X, MatrixXd Y, VectorXd B)
+MatrixXd theHessian(MatrixXd X, VectorXd Y, VectorXd B)
 {
     VectorXd regressionLine = X.transpose()*B;
     ArrayXd probs = 1 / (1 + ( -regressionLine).array().exp() );
@@ -55,9 +57,69 @@ MatrixXd theHessian(MatrixXd X, MatrixXd Y, VectorXd B)
     return result;
 }
 
+bool getData(const string& filename, MatrixXd& X, VectorXd& Y, VectorXd& B)
+{
+    ifstream file(filename);
+
+    if(!file.is_open())
+    {
+        cout << "Couldn't open csv file" << endl;
+        return false;
+    }
+
+    vector<vector<double>> rawData;
+    string line;
+
+    bool isHeader = true;
+    while(getline(file, line))
+    {
+        if (isHeader)
+        {
+            isHeader = false; // skip the first row 
+            continue;
+        }
+        stringstream lineStream(line);
+        string cell;
+        vector<double> row;
+
+        while(getline(lineStream, cell, ','))
+            row.push_back(stod(cell));
+        rawData.push_back(row);
+    }
+
+    file.close();
+
+    int rows = rawData.size();
+    int cols = rawData[0].size();
+
+    X.resize(rows - 1, cols);
+    for(int i = 0; i < rows - 1; ++i)
+    {
+        for(int j = 0; j < cols; ++j)
+        {
+            X(i,j) = rawData[i][j];
+        }
+    }
+
+    for(int k = 0; k < cols; ++k)
+        Y(k) = rawData[rows - 1][k];
+    Y.resize(rows - 1);
+
+    for(int l = 0; l < cols; ++l)
+        B(l) = l + 1;
+    B.resize(cols);
+    return true;
+}
+
 int main()
 {
-    // :D
+    MatrixXd X;
+    VectorXd Y;
+    VectorXd B;
+
+    string filename = "SyntheticLRdata.csv";
+    getData(filename, X, Y, B);
+    newtonRaphson( B, X, Y, 0.1);
 
     return 0;
 }
